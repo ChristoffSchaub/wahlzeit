d81f280dbb97d3a100af490f663bebe1a3412996
ChristoffSchaub <christoff.schaub@gmail.com>
2020-11-13 09:43:23 +0100

    Added public coordinate attribute to Location

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -1,4 +1,10 @@
 package org.wahlzeit.model;
 
 public class Location {
+
+    public Coordinate coordinate;
+
+    public Location(Coordinate coordinate) {
+        this.coordinate = coordinate;
+    }
 }

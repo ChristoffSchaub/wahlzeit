1a9542e3d7b1cb6448fa4c8561f8e46c409508d5
ChristoffSchaub <christoff.schaub@gmail.com>
2020-11-13 09:38:41 +0100

    added Coordination  class to Project

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,4 +1,14 @@
 package org.wahlzeit.model;
 
 public class Coordinate {
+
+    public Coordinate(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    private double x;
+    private double y;
+    private double z;
 }

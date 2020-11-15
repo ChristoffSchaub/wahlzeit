0ea93aa507f776cb099d99d6dfd8da4fce166916
ChristoffSchaub <christoff.schaub@gmail.com>
2020-11-15 12:13:32 +0100

    Refactoring and Forwarding equals

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -2,16 +2,38 @@ package org.wahlzeit.model;
 
 public class Coordinate {
 
+    private double x;
+    private double y;
+    private double z;
+
     public Coordinate(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    private double x;
-    private double y;
-    private double z;
 
+
+    public double getDistance(Coordinate coordinate) throws NullPointerException{
+        if (coordinate == null)
+            throw  new NullPointerException("coordinate can not be null");
+        return Math.sqrt(Math.pow(coordinate.getX() - this.getX(), 2) + Math.pow(coordinate.getY() - this.getY(), 2) + Math.pow(coordinate.getZ() - this.getZ(), 2));
+    }
+
+    public boolean isEqual(Coordinate coordinate) {
+        if (coordinate == null)
+            return false;
+        if (this == coordinate) {
+            return true;
+        } else if (Double.compare(coordinate.getZ(), this.getZ())==0
+                && Double.compare(coordinate.getY(), this.getY())==0
+                && Double.compare(coordinate.getX(), this.getX())==0
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
     public double getX() {
         return x;
     }
@@ -35,24 +57,9 @@ public class Coordinate {
     public void setZ(double z) {
         this.z = z;
     }
-
-    public double getDistance(Coordinate coordinate) {
-        return Math.sqrt(Math.pow(coordinate.getX() - this.getX(), 2) + Math.pow(coordinate.getY() - this.getY(), 2) + Math.pow(coordinate.getZ() - this.getZ(), 2));
+    public boolean equals(Object o){
+        if(o instanceof Coordinate)
+            return this.isEqual((Coordinate) o );
+        return false;
     }
-
-    public boolean isEqual(Coordinate coordinate) {
-        if (coordinate == null)
-            return false;
-        if (this == coordinate) {
-            return true;
-        } else if (coordinate.getZ() == this.getZ()
-                && coordinate.getY() == this.getY()
-                && coordinate.getX() == this.getX()
-        ) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
 }

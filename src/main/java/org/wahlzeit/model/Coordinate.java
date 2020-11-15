885fa211db6c37ce282b6f8ed6ec191c8953ce7c
ChristoffSchaub <christoff.schaub@gmail.com>
2020-11-15 10:12:52 +0100

    Added getter and setter for Location in Photo.java and implemented getDistance and isEqual

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -11,4 +11,48 @@ public class Coordinate {
     private double x;
     private double y;
     private double z;
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    public double getDistance(Coordinate coordinate) {
+        return Math.sqrt(Math.pow(coordinate.getX() - this.getX(), 2) + Math.pow(coordinate.getY() - this.getY(), 2) + Math.pow(coordinate.getZ() - this.getZ(), 2));
+    }
+
+    public boolean isEqual(Coordinate coordinate) {
+        if (coordinate == null)
+            return false;
+        if (this == coordinate) {
+            return true;
+        } else if (coordinate.getZ() == this.getZ()
+                && coordinate.getY() == this.getY()
+                && coordinate.getX() == this.getX()
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }

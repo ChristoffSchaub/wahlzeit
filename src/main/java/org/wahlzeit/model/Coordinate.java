f2158cea6867d1bddb85a717773035e998a81924
ChristoffSchaub <christoff.schaub@gmail.com>
2020-11-22 16:06:43 +0100

    Refactoring isEqual and adding hashCode Method

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,5 +1,7 @@
 package org.wahlzeit.model;
 
+import java.util.Objects;
+
 public class Coordinate {
 
     private double x;
@@ -13,27 +15,35 @@ public class Coordinate {
     }
 
 
-
-    public double getDistance(Coordinate coordinate) throws NullPointerException{
+    public double getDistance(Coordinate coordinate) throws NullPointerException {
         if (coordinate == null)
-            throw  new NullPointerException("coordinate can not be null");
+            throw new NullPointerException("coordinate can not be null");
         return Math.sqrt(Math.pow(coordinate.getX() - this.getX(), 2) + Math.pow(coordinate.getY() - this.getY(), 2) + Math.pow(coordinate.getZ() - this.getZ(), 2));
     }
 
     public boolean isEqual(Coordinate coordinate) {
-        if (coordinate == null)
-            return false;
-        if (this == coordinate) {
-            return true;
-        } else if (Double.compare(coordinate.getZ(), this.getZ())==0
-                && Double.compare(coordinate.getY(), this.getY())==0
-                && Double.compare(coordinate.getX(), this.getX())==0
-        ) {
-            return true;
-        } else {
+        double threshold=0.000001;
+
+        if (coordinate == null) {
             return false;
         }
+
+        if (this == coordinate) {
+            return true;
+        }
+
+        boolean eqX = Math.abs(coordinate.getX() - this.getX())>threshold?true:false;
+        boolean eqY = Math.abs(coordinate.getY() - this.getY())>threshold?true:false;
+        boolean eqZ = Math.abs(coordinate.getZ() - this.getZ())>threshold?true:false;
+
+        return eqX && eqY && eqZ;
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY(), getZ());
+    }
+
     public double getX() {
         return x;
     }
@@ -57,9 +67,11 @@ public class Coordinate {
     public void setZ(double z) {
         this.z = z;
     }
-    public boolean equals(Object o){
-        if(o instanceof Coordinate)
-            return this.isEqual((Coordinate) o );
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Coordinate)
+            return this.isEqual((Coordinate) o);
         return false;
     }
 }
